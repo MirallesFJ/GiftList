a0249a293c57afc37d784b335a02b6e62c2d0527
miralles <jorge.miralles@itq.de>
2024-10-05 16:46:08 +0200

    solved exercise.

--- client/index.js
+++ client/index.js
@@ -1,14 +1,26 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const verifyProof = require("../utils/verifyProof");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  // create the merkle tree for the whole nice list
+  const merkleTree = new MerkleTree(niceList);
+
+  // get the root
+  const root = merkleTree.getRoot();
+
+  // find the proof that norman block is in the list
+  const name = "Norman Block";
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
   });
 
   console.log({ gift });
